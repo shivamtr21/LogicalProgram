57b477b2ab9fa3a4eaa725cd2267b63a518d3e5e
shivamtr21 <shivamtripathi26297@gmail.com>
2023-03-15 15:25:35 +0530

    Find the Quotient and remaider

---
+++ QuotientAndRemainder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProblems
+{
+    public class QuotientAndRemainder
+    {
+        public static void FindQuotientRemainder()
+        {
+            int dividend, divisor;
+
+            Console.WriteLine("Enter a value for dividend");
+            dividend = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter a value for divisor");
+            divisor = Convert.ToInt32(Console.ReadLine());
+
+
+            int remainder = dividend % divisor;
+            int Quotient = dividend / divisor;
+
+            Console.WriteLine("dividend " + dividend + " and divisor " + divisor);
+            Console.WriteLine("the quotient is " + Quotient + " and the remainder is " + remainder);
+        }
+    }
+}
