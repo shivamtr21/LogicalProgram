fa48f6e58bd2b7ad0b97cd3f8eb31d4987a2cd03
shivamtr21 <shivamtripathi26297@gmail.com>
2023-03-15 15:18:46 +0530

    Check the given character is vowel or Consonant

---
+++ VowelConsonant.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProblems
+{
+    public class VowelConsonant
+    {
+        public static void CheckVowelConsonant()
+        {
+            Console.WriteLine("Enter the character");
+
+            char A = Convert.ToChar(Console.ReadLine());
+
+            if (A == 'a' || A == 'e' || A == 'i' || A == 'o' || A == 'u' || A == 'A' || A == 'E' || A == 'I' || A == 'O' || A == 'U')
+            {
+                Console.WriteLine(A + " is Vowel");
+            }
+            else if (( A >= 'a' && A <= 'z') || (A >= 'A' && A <= 'Z'))
+            {
+                Console.WriteLine(A + "  is Consonant");
+            }
+
+            else
+            {
+                Console.WriteLine("Please Enter a valid character");
+            }
+        }
+    }
+}
