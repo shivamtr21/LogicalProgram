58e044f27a72c95378e897477329bc9ae71fc882
shivamtr21 <shivamtripathi26297@gmail.com>
2023-03-15 15:07:48 +0530

    Welcome to the Logical Program

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace LogicalProblems
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Logical Program");
+            //EvenOdd.CheckEvenOdd();
+            // LargestNumber.CheckLargestNumber();
+            //VowelConsonant.CheckVowelConsonant();
+            // QuotientAndRemainder.FindQuotientRemainder();
+            //SwappingNumber.SwapTheNumber();
+        }
+    }
+}
