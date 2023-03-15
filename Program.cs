05a0f583056f4eb8d6c2396c52c48ed57df1731f
shivamtr21 <shivamtripathi26297@gmail.com>
2023-03-15 15:12:21 +0530

    Find the given number is even or odd

--- Program.cs
+++ Program.cs
@@ -4,12 +4,9 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Logical Program");
-            //EvenOdd.CheckEvenOdd();
-            // LargestNumber.CheckLargestNumber();
-            //VowelConsonant.CheckVowelConsonant();
-            // QuotientAndRemainder.FindQuotientRemainder();
-            //SwappingNumber.SwapTheNumber();
+            EvenOdd.CheckEvenOdd();
+
+
         }
     }
 }
